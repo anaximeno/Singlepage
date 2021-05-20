8996b6b5d335b86d94ea59f291207b294251ae00
anaximeno <anaximenobrito@gmail.com>
2021-05-20 00:11:00 -0100

    some good updates

--- js/index.js
+++ js/index.js
@@ -1,18 +1,21 @@
 // Next/previous controls
-function plusSlides(n) {
+function plusSlides(n)
+{
   clearTimeout(m);
   slideIndex = slideIndex + n;
   showSlides(slideIndex);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
+function currentSlide(n)
+{
   showSlides(n);
 }
 
 var m, slideIndex = 0;
 
-function showSlides() {
+function showSlides()
+{
   var i, s;
 
   var slides = $(".mySlides");
@@ -28,79 +31,84 @@ function showSlides() {
 }
 
 
-$(document).ready(function(){
-
-    /* Night/Day mode */
+$(document).ready(() => {
+    var d = new Date();
+    var hour = d.getHours();
+    var day = (hour >= 18 || hour < 6) ? false : true;
 
     var logo = $('#logo');
     var bulbs = $('.bulbs img');
-    var bls = document.querySelector(".bulbs").querySelectorAll("img");
-    var bd = $(".fullbody");
+    var centralbox = $(".fullbody");
     var nav = $(".nav");
     var assunto1 = $("#assunto-1");
-    var abt = $("#about");
-    var it = $("#init .text");
+    var about = $("#about");
+    var texts = $("#intexts .text");
     var body = $("body");
-    var hf = $(".header, .footer, .footer-down, .header-down");
-    var hdown = $(".header-down");
-    var fdown = $(".footer-down");
-    var hdr = $(".header");
-    var fdr = $(".footer");
+    var header_and_footer = $(".header, .footer, .footer-down, .header-down");
+    var header = $(".header");
+    var footer = $(".footer");
 
-    function nightMode(){
-        day = false;
-
-        for(var i = 0; i < bulbs.length; i++) {
-          bls[i].src = "./imagens/bulboff.svg";
-          bls[i].title = "Go to Day Mode";
-        }
-
-        hf.css("background-color", "#222");
-        hf.css("color", "white");
-        hdr.css("box-shadow", "2px -1px 19px 9px rgba(0,0,0,0.7)");
-        fdr.css("box-shadow", "2px 4px 19px 9px rgba(0,0,0,0.7)");
-        it.css("color", "#232323");
-        logo.css("color", "white");
-        bd.css("background-color", "#232323");
-        assunto1.css("color", "white");
-        abt.css("color", "white");
-        nav.css("color", "white");
-        $("#assunto-2 .text").css("color", "#232323");
-        body.css("background-image", "linear-gradient(60deg, #29323c 0%, #485563 100%)");
-    }
-
-    function dayMode(){
-        day = true;
-
-        for(var i = 0; i < bulbs.length; i++) {
-          bls[i].src = "./imagens/bulbon.svg";
-          bls[i].title = "Go to Night Mode";
-        }
-
-        hf.css("background-color", "white");
-        hf.css("color", "#232323");
-        hdr.css("box-shadow", "none");
-        fdr.css("box-shadow", "none");
-        it.css("color", "white");
-        logo.css("color", "#232323");
-        bd.css("background-color", "white");
-        assunto1.css("color", "#232323");
-        abt.css("color", "#232323");
-        nav.css("color", "#232323");
-        $("#assunto-2 .text").css("color", "white");
-        body.css("background-image", "none");
-    }
-
-    var d = new Date();
-    var hour = d.getHours();
-
-    var day;
-
-    if(hour >= 18 || hour < 6) nightMode();
-    else day = true;
-
-    bulbs.click(function() {
-        if(day) nightMode();  // change to night mode
-        else dayMode();       // change to day mode
+    // TODO: show an animation before site end charging.
+    window.addEventListener('load', () => {
+        if (!day) 
+            nightMode();
     });
+
+    bulbs.click(() => {
+        day = day ? nightMode() : dayMode();
+    });
+
+    function change_bulbs(src, ttextsle)
+    {
+        var bls = document.querySelector(".bulbs").querySelectorAll("img");
+        for(var i = 0; i < bulbs.length; i++) {
+            bls[i].src = src;
+            bls[i].ttextsle = ttextsle;
+        }
+    }
+
+
+    function change_colors(colors)
+    {
+        header_and_footer.css("background-color", colors.c3);
+        header_and_footer.css("color", colors.c1);
+        header.css("box-shadow", colors.shadow1);
+        footer.css("box-shadow", colors.shadow2);
+        texts.css("color", colors.c2);
+        logo.css("color", colors.c1);
+        centralbox.css("background-color", colors.c2);
+        assunto1.css("color", colors.c1);
+        about.css("color", colors.c1);
+        nav.css("color", colors.c1);
+        $("#assunto-2 .text").css("color", colors.c2);
+        body.css("background", colors.background);
+    }
+
+    function nightMode(colors)
+    {
+        change_bulbs("./imagens/bulboff.svg", "Go to Day Mode");
+        change_colors({
+            c1: "#fff",
+            c2: "#191919",
+            c3: "#121212",
+            background: "#101010",
+            shadow1: "2px -1px 19px 9px rgba(0,0,0,0.9)",
+            shadow2: "2px 4px 19px 9px rgba(0,0,0,0.9)"
+        });
+        return false;
+    }
+
+    function dayMode()
+    {
+        change_bulbs("./imagens/bulbon.svg", "Go to Night Mode");
+        change_colors({
+            c1: "#191919",
+            c2: "#fff",
+            c3: "#fff",
+            background: "#fff",
+            shadow1: "none",
+            shadow2: "none"
+        });
+        return true;
+    }
 });
