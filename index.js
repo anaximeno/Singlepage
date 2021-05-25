83a043d5d8727c0fade28104c98b6a8826a09042
anaximeno <anaximenobrito@gmail.com>
2021-05-25 00:30:26 -0100

    Make some fixes and updates

--- index.js
+++ index.js
@@ -4,36 +4,38 @@ function sleep(ms)
 }
 
 
-function SlideShower(slides, changetime, reference)
+class SlideShower 
 {
-    this.slides = slides;
-    this.changetime = changetime;
-    this.ref = reference;
-    this.current_index = 0;
-    this.go_next = () => {
-        this.current_index++;
-        if (this.current_index < 0 || this.current_index >= this.slides.length)
-            this.current_index = 0;
-    }
-    this.go_prev = () => {
-        this.current_index--;
-        if (this.current_index < 0 || this.current_index >= this.slides.length)
-            this.current_index = this.slides.length - 1;
-    }
-    this.show = () => {
-        var slide_img = document.getElementById(this.ref.image);
-        var slide_text = document.getElementById(this.ref.text);
-        var slide_num = document.getElementById(this.ref.num);
+    constructor(slides, changetime, reference) {
+        this.slides = slides;
+        this.changetime = changetime;
+        this.ref = reference;
+        this.current_index = 0;
+        this.go_next = () => {
+            this.current_index++;
+            if (this.current_index < 0 || this.current_index >= this.slides.length)
+                this.current_index = 0;
+        };
+        this.go_prev = () => {
+            this.current_index--;
+            if (this.current_index < 0 || this.current_index >= this.slides.length)
+                this.current_index = this.slides.length - 1;
+        };
+        this.show = () => {
+            var slide_img = document.getElementById(this.ref.image);
+            var slide_text = document.getElementById(this.ref.text);
+            var slide_num = document.getElementById(this.ref.num);
 
-        slide_img.src = this.slides[this.current_index].src;
-        slide_text.innerHTML = this.slides[this.current_index].text;
-        slide_num.innerHTML = (this.current_index+1) + '/' + this.slides.length;
-        this.go_next();
-    }
-    this.animate_slides = async () => {
-        do {
-            this.show();
-            await sleep(this.changetime);
-        } while (true);
+            slide_img.src = this.slides[this.current_index].src;
+            slide_text.innerHTML = this.slides[this.current_index].text;
+            slide_num.innerHTML = (this.current_index + 1) + '/' + this.slides.length;
+            this.go_next();
+        };
+        this.animate_slides = async () => {
+            do {
+                this.show();
+                await sleep(this.changetime);
+            } while (true);
+        };
     }
 }
