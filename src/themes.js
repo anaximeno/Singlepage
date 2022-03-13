3139caee4e5bff9e552ec0d47aff8f3e02884bd2
anaximeno <anaximenobrito@gmail.com>
2022-03-13 00:13:00 -0100

    made some fixes on the interface

--- src/themes.js
+++ src/themes.js
@@ -1,3 +1,4 @@
+/* Here was taken a more functional approach */
 let lightModeEnabled;
 
 const MOON = Symbol("moon");
@@ -11,7 +12,7 @@ const DARK_BLACKEY_THEME = {
     back03: "#121212",
     shadow01: "2px -1px 19px 9px rgba(0,0,0,0.9)",
     shadow02: "2px 4px 19px 9px rgba(0,0,0,0.9)",
-    iconRef: "moon"
+    iconRef: "#moon"
 }
 
 
@@ -22,16 +23,19 @@ const LIGHT_WHITEY_THEME = {
     back03: "#fff",
     shadow01: "none",
     shadow02: "none",
-    iconRef: "sun"
+    iconRef: "#sun"
 }
 
 
-const ELEMENTS_REFERENCIES = {
-    texts: '#logo, .text, #assunto-1, #about, .nav, \
-    .header, .footer, .footer-down, .header-down, #assunto-2, button',
+const REFS = {
+    texts: `#logo, .text, #assunto-1, 
+            #about, .nav, .nav a, .footer a,
+            .header, .footer, .footer-down, 
+            .header-down, #assunto-2, button`,
     back01: 'body',
     back02: '.fullbody',
-    back03: '.header, .footer, .footer-down, .header-down',
+    back03: `.header, .footer, 
+            .footer-down, .header-down`,
     shadow01: '.header',
     shadow02: '.footer'
 }
@@ -44,7 +48,7 @@ const isDayTime = () => {
 
 
 const changeThemeTo = (theme) => {
-    const elements = ELEMENTS_REFERENCIES;
+    const elements = REFS;
     $(elements.texts).css('color', theme.texts);
     $(elements.back01).css('background-color', theme.back01);
     $(elements.back02).css('background-color', theme.back02);
@@ -55,20 +59,19 @@ const changeThemeTo = (theme) => {
 
 
 const changeIconTo = (symbol) => {
-    const sun = document.getElementById(LIGHT_WHITEY_THEME.iconRef);
-    const moon = document.getElementById(DARK_BLACKEY_THEME.iconRef);
-    switch (symbol) {
-        case SUN:
-            sun.hidden = false;
-            moon.hidden = true;
-            break;
-        case MOON:
-            sun.hidden = true;
-            moon.hidden = false;
-            break;
-        default:
-            throw TypeError("Wrong type for changeIconTo, expected SUN or MOON");
-    }    
+    const sun = $(LIGHT_WHITEY_THEME.iconRef);
+    const moon = $(DARK_BLACKEY_THEME.iconRef);
+    if (symbol == SUN) {
+        moon.hide();
+        sun.show();
+    } else if (symbol == MOON) {
+        sun.hide();
+        moon.show();
+    } else {
+        throw TypeError(
+            "Wrong type for changeIconTo, expected SUN or MOON"
+        );
+    }
 }
 
 
@@ -85,10 +88,14 @@ const lightMode = () => {
 
 
 const changeMode = async (animate = true, sleepTime = 200) => {
-    const themeIcon = '.icon';
+    const iconClass = '.icon';
     if (animate === true) {
-        gsap.to(themeIcon, {duration: .15, opacity: 0});
-        gsap.to(themeIcon, {
+        gsap.to(iconClass, {
+            duration: .15,
+            opacity: 0
+        });
+
+        gsap.to(iconClass, {
             duration: .19,
             x: "50px",
             y: "-50px",
@@ -96,17 +103,25 @@ const changeMode = async (animate = true, sleepTime = 200) => {
         });
 
         await sleep(sleepTime * 0.6);
+
         changeIconTo(lightModeEnabled ? MOON : SUN);
+
         await sleep(sleepTime * 0.4);
 
-        gsap.to(themeIcon, {duration: .17, opacity: 1, delay: 0.19});
-        gsap.to(themeIcon, {
+        gsap.to(iconClass, {
+            duration: .17,
+            opacity: 1,
+            delay: 0.19
+        });
+
+        gsap.to(iconClass, {
             duration: .19,
             x: "0px",
             y: "0px",
             delay: 0.19,
             ease: "circ.out"
         });
+
         await sleep(sleepTime * 1.25);
     } else {
         changeIconTo(lightModeEnabled ? MOON : SUN);
